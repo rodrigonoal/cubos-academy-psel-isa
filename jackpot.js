c4e32c08e2352c470e1b0b369441d3e370cd88f9
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-03-09 20:55:44 -0300

    All my friends are seven

---
+++ jackpot.js
@@ -0,0 +1,10 @@
+function solucao(resultado1, resultado2, resultado3) {
+	// seu código aqui
+    
+    if (resultado1 === resultado2 && resultado1 === resultado3){
+        console.log(true);
+    }
+        else {
+            console.log(false)
+        }
+}
