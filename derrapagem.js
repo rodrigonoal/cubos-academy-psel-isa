7bb7f79d345b70b8fc7cff8948332943324cfd21
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-03-09 20:57:59 -0300

    Run, Forrest, Run!

---
+++ derrapagem.js
@@ -0,0 +1,12 @@
+
+function solucao(velocidade, aceleracao) {
+	//seu código aqui  
+    
+    let dist = - (velocidade * velocidade) / (2 * (aceleracao))
+    
+    console.log(dist)
+    
+       
+    
+  
+}
