9e924346c3d50eb0a8d43d07c06e9742ccff88e1
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-03-09 21:04:47 -0300

    Like a virgin

---
+++ purificacao.js
@@ -0,0 +1,48 @@
+function solucao(stringCorrompida) {
+	// seu código aqui
+   
+    
+    
+    let resposta = ""
+
+for (i = 0; i < stringCorrompida.length; i++) {
+  const piece = stringCorrompida[i];
+ if (piece === "!") {
+    resposta += "";
+  }
+  else if (piece === "@") {
+    resposta += "";
+  }
+  else if (piece === "#") {
+    resposta += "";
+  }
+  else if (piece === "$") {
+    resposta += "";
+  }
+  else if (piece === "%") {
+    resposta += "";
+  }
+  else if (piece === "&") {
+    resposta += "";
+  }
+    else if (piece === "*") {
+    resposta += "";
+    }
+else if (piece === "(") {
+    resposta += "";
+}
+else if (piece === ")") {
+    resposta += "";
+  }
+  else {
+    resposta += piece;
+  }
+
+}
+ console.log(resposta);
+    
+    
+    
+    
+    
+}
